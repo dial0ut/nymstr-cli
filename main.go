a45faae489a2bd4af26778324ff1cbbba039fe57
Hans Bricks <hans@dialout.net>
2023-06-02 19:27:31 +0200

    somewhat working cli app menu and successfuly sends message

--- main.go
+++ main.go
@@ -5,8 +5,14 @@ import (
     "github.com/abiosoft/ishell"
     "github.com/gorilla/websocket"
     "encoding/json"
-    "strconv"
+    "os"
+    "os/exec"
+    "io/ioutil"
+    "log"
+    "strings"
 )
+
+const contactFile = "$HOME/.nym/clients/nymstr-chat/config/contacts.lst"
 
 type NymCLI struct {
     *ishell.Shell
@@ -50,6 +56,7 @@ func New(uri string) *NymCLI {
     return cli
 }
 
+
 func (cli *NymCLI) sendCommand(c *ishell.Context) {
     if len(cli.contacts) == 0 {
         fmt.Println("No contacts in the list. First add an address with `contacts add <address>`")
@@ -81,7 +88,6 @@ func (cli *NymCLI) sendCommand(c *ishell.Context) {
         panic(err)
     }
 }
-
 func (cli *NymCLI) contactsCommand(c *ishell.Context) {
     if len(c.Args) == 0 {
         for name, address := range cli.contacts {
@@ -98,6 +104,24 @@ func (cli *NymCLI) contactsCommand(c *ishell.Context) {
     } else {
         fmt.Println("usage: contacts add <name> <address>")
     }
+}
+func (cli *NymCLI) getSelfAddress() string {
+    selfAddressRequest, err := json.Marshal(map[string]string{"type": "selfAddress"})
+    if err != nil {
+        panic(err)
+    }
+
+    if err = cli.conn.WriteMessage(websocket.TextMessage, []byte(selfAddressRequest)); err != nil {
+        panic(err)
+    }
+
+    responseJSON := make(map[string]interface{})
+    err = cli.conn.ReadJSON(&responseJSON)
+    if err != nil {
+        panic(err)
+    }
+
+    return responseJSON["address"].(string)
 }
 
 func (cli *NymCLI) testCommand(c *ishell.Context) {
@@ -120,39 +144,28 @@ func (cli *NymCLI) testCommand(c *ishell.Context) {
         panic(err)
     }
 }
-
-func (cli *NymCLI) getSelfAddress() string {
-    selfAddressRequest, err := json.Marshal(map[string]string{"type": "selfAddress"})
-    if err != nil {
-        panic(err)
-    }
-
-    if err = cli.conn.WriteMessage(websocket.TextMessage, []byte(selfAddressRequest)); err != nil {
-        panic(err)
-    }
-
-    responseJSON := make(map[string]interface{})
-    err = cli.conn.ReadJSON(&responseJSON)
-    if err != nil {
-        panic(err)
-    }
-
-    return responseJSON["address"].(string)
-}
+// ...
 
 // Launch nym-client if it is not running and exit if connection is not possible
-func (cli *NymCLI) ConnectOrStartNymClient() error {
-    conn, _, err := websocket.DefaultDialer.Dial(cli.uri, nil)
+func (cli *NymCLI) ConnectOrStartNymClient(uri string) error {
+    conn, _, err := websocket.DefaultDialer.Dial(uri, nil)
     if err == nil {
         // we managed to connect, so we close the connection and return
         conn.Close()
         return nil
     }
     // there was an error connecting, so we try to start nym-client
-    cmd := exec.Command("./nym-client")
+
+    logFile, err := os.Create("/path/to/your/project/error.log")
+    if err != nil {
+        return fmt.Errorf("failed to create log file: %w", err)
+    }
+
+    cmd := exec.Command("./nym-client", "run", "--id", "nymstr-cli")
     cmd.Dir = "." // this is the directory in which the command will be run
-    cmd.Stdout = os.Stdout
-    cmd.Stderr = os.Stderr
+    cmd.Stdout = logFile
+    cmd.Stderr = logFile
+
     err = cmd.Start()
     if err != nil {
         return fmt.Errorf("could not start nym-client: %w", err)
@@ -164,6 +177,36 @@ func (cli *NymCLI) ConnectOrStartNymClient() error {
         return fmt.Errorf("nym-client exited with error: %w", err)
     }
     return nil
+}
+
+func saveContacts(contacts []string) {
+    // Convert the slice of strings into a single string with line breaks
+    content := strings.Join(contacts, "\n")
+
+    // Write the content to the file
+    err := ioutil.WriteFile(os.ExpandEnv(contactFile), []byte(content), 0644)
+    if err != nil {
+        log.Fatalf("Error writing to file: %v", err)
+    }
+}
+
+func readContacts() []string {
+    // Check if the file exists
+    if _, err := os.Stat(os.ExpandEnv(contactFile)); os.IsNotExist(err) {
+        // File does not exist, return an empty slice
+        return []string{}
+    }
+
+    // Read the file
+    content, err := ioutil.ReadFile(os.ExpandEnv(contactFile))
+    if err != nil {
+        log.Fatalf("Error reading file: %v", err)
+    }
+
+    // Split the content by line break to get the contacts
+    contacts := strings.Split(string(content), "\n")
+
+    return contacts
 }
 
 func main() {
